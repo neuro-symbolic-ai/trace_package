b36fc9386847e9767be1b6475b528497e40f768a
Nura J <nura.aljaafari@postgrad.manchester.ac.uk>
2025-06-10 12:06:43 +0100

    Initial training init file

---
+++ trace/training/__init__.py
@@ -0,0 +1,38 @@
+from .config import TrainingConfig
+from .trainer import Trainer, train_model
+from .callbacks import TrainingCallbacks
+from .utils import (
+    prepare_batch_for_model,
+    compute_loss,
+    setup_hidden_state_hooks,
+    get_device_from_string,
+    set_seed,
+    save_checkpoint,
+    validate_model,
+    MODEL_TYPE_ENCODER_ONLY,
+    MODEL_TYPE_DECODER_ONLY,
+    MODEL_TYPE_ENCODER_DECODER,
+    TASK_MODE_MLM,
+    TASK_MODE_NEXT_TOKEN,
+    TASK_MODE_SEQ2SEQ
+)
+
+__all__ = [
+    'TrainingConfig',
+    'Trainer',
+    'train_model',
+    'TrainingCallbacks',
+    'prepare_batch_for_model',
+    'compute_loss',
+    'setup_hidden_state_hooks',
+    'get_device_from_string',
+    'set_seed',
+    'save_checkpoint',
+    'validate_model',
+    'MODEL_TYPE_ENCODER_ONLY',
+    'MODEL_TYPE_DECODER_ONLY',
+    'MODEL_TYPE_ENCODER_DECODER',
+    'TASK_MODE_MLM',
+    'TASK_MODE_NEXT_TOKEN',
+    'TASK_MODE_SEQ2SEQ'
+]
