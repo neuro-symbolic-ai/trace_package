d62f9dfb314519d00e4cab2411db4af89208ee08
Nura J <nura.aljaafari@postgrad.manchester.ac.uk>
2025-06-25 09:32:31 +0100

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,90 @@
+# from setuptools import setup
+# import os
+#
+# PKG_ROOT = os.path.abspath(os.path.dirname(__file__))
+#
+#
+# def load_requirements():
+#     """Load requirements from file, separating git repos from regular packages"""
+#     with open(os.path.join(PKG_ROOT, "requirements.txt"), encoding="utf-8") as f:
+#         all_reqs = f.read().split("\n")
+#
+#     install_requires = []
+#     dependency_links = []
+#
+#     for req in all_reqs:
+#         req = req.strip()
+#         if req and not req.startswith('#'):
+#             if req.startswith('git+'):
+#                 # Extract package name from git URL if possible
+#                 # For git+https://github.com/nura-j/ABSynth_dataset.git
+#                 # We can add it as a dependency link
+#                 dependency_links.append(req)
+#                 # You might need to add the package name to install_requires
+#                 # install_requires.append('ABSynth_dataset')  # uncomment if needed
+#             else:
+#                 install_requires.append(req)
+#
+#     return install_requires, dependency_links
+#
+#
+# install_requires, dependency_links = load_requirements()
+#
+# setup(
+#     name='trace',
+#     version='0.1',
+#     packages=['trace', 'trace.hessian', 'trace.transformer', 'trace.linguistic_probes',
+#               'trace.intrisic_dimensions', 'trace.tokenizer', 'trace.utils', 'trace.dataloader'],
+#     url='',
+#     license='',
+#     author='nura_aljaafari',
+#     author_email='nuraaljaafari@gmail.com',
+#     description='',
+#     install_requires=install_requires,
+#     dependency_links=dependency_links
+# )
 from setuptools import setup
 import os
+
 PKG_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-def load_requirements() -> list:
-    """Load requirements from file, parse them as a Python list"""
 
+def load_requirements():
+    """Load requirements from file, separating git repos from regular packages"""
     with open(os.path.join(PKG_ROOT, "requirements.txt"), encoding="utf-8") as f:
         all_reqs = f.read().split("\n")
-    install_requires = [str(x).strip() for x in all_reqs]
 
-    return install_requires
+    install_requires = []
+    dependency_links = []
+
+    for req in all_reqs:
+        req = req.strip()
+        if req and not req.startswith('#'):
+            if req.startswith('git+'):
+                # Extract package name from git URL if possible
+                # For git+https://github.com/nura-j/ABSynth_dataset.git
+                # We can add it as a dependency link
+                dependency_links.append(req)
+                # You might need to add the package name to install_requires
+                # install_requires.append('ABSynth_dataset')  # uncomment if needed
+            else:
+                install_requires.append(req)
+
+    return install_requires, dependency_links
+
+
+install_requires, dependency_links = load_requirements()
 
 setup(
     name='trace',
     version='0.1',
-    packages=['trace', 'trace.hessian', 'trace.transformer', 'trace.linguistic_probes', 'trace.intrisic_dimensions',
-              'trace.tokenizer', 'trace.utils', 'trace.dataloader'],
+    packages=['trace', 'trace.hessian', 'trace.transformer', 'trace.linguistic_probes',
+              'trace.intrisic_dimensions', 'trace.tokenizer', 'trace.utils', 'trace.dataloader'],
     url='',
     license='',
     author='nura_aljaafari',
     author_email='nuraaljaafari@gmail.com',
     description='',
-    install_requires=load_requirements()
+    install_requires=install_requires,
+    dependency_links=dependency_links
 )
