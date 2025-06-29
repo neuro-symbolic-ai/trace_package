d8de32bb148c67915b86b9664248700e1a3a6c14
Nura J <nura.aljaafari@postgrad.manchester.ac.uk>
2025-06-29 21:04:01 +0100

    adding debugging code

--- trace/training/trainer.py
+++ trace/training/trainer.py
@@ -256,6 +256,11 @@ class Trainer:
             # exit(0)
             # Backward pass
             loss.backward()
+            print("=== OPTIMIZER STATE ===")
+            print("Optimizer type:", type(self.optimizer))
+            print("Learning rate:", self.optimizer.param_groups[0]['lr'])
+            print("Weight decay:", self.optimizer.param_groups[0].get('weight_decay', 'None'))
+            print("Optimizer param groups:", len(self.optimizer.param_groups))
             # print("Batch keys:", batch.keys())
             # print("Input shape:", batch["input_ids"].shape)
             # print("Input sample:", batch["input_ids"][0][:10])
