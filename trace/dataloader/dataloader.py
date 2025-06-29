7aa03bb478220db3014499c7495d592beecec35d
Nura J <nura.aljaafari@postgrad.manchester.ac.uk>
2025-06-29 19:22:23 +0100

    adding debugging code- manual seed

--- trace/dataloader/dataloader.py
+++ trace/dataloader/dataloader.py
@@ -298,7 +298,8 @@ def get_dataloader(corpus_path,
     test_size = int(total_size * test_split)
     val_size = int(total_size * val_split)
     train_size = total_size - val_size - test_size
-    torch.manual_seed(42)  # Or use config.seed
+    torch.manual_seed(42)
+
     train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True) if train_size > 0 else None
